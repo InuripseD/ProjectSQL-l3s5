cebb0e154f08640b18c93f0a4b7e92ba6b79bd13
Matteo Faura-Behague <matteo.faura-behague@etu.umontpellier.fr>
2022-12-14 10:09:18 +0100

    les triggers fonctionnents

--- remplissage_Groupe3.sql
+++ remplissage_Groupe3.sql
@@ -4,38 +4,14 @@ SET PAGESIZE 30
 COLUMN COLUMN_NAME FORMAT A30
 SET LINESIZE 300
 
-/*
-Effacer les anciennes valeurs des relations
-*/
-
-prompt -------------------------------------------;
-prompt --- Suppression des anciens tuples --------;
-prompt -------------------------------------------;
-
-DELETE FROM TRANSFUSE;
-DELETE FROM TRAITE;
-DELETE FROM DON;
-DELETE FROM COLLECTE;
-DELETE FROM HOPITAL;
-DELETE FROM PATIENT;
-DELETE FROM PERSONNEL;
-DELETE FROM DONNEUR;
-DELETE FROM PERSONNE;
-
-drop procedure INSERTINTODONEUR;
-
 prompt -------------------------------------------;
 prompt --- Insertion des nouveaux tuples ---------;
 prompt -------------------------------------------;
 
-/*
-Creer une fonctions pour insert à la place des insertinto
-*/
 
 prompt ------------------------------------------;
 prompt ---------- insertion DONNEUR -------------;
 prompt ------------------------------------------;
-
 
 
 prompt ------------------------------------------;
@@ -55,7 +31,7 @@ INSERT INTO HOPITAL VALUES (10,'Clinique Medipole Garonne','45 Rue de Gironis, 3
 
 
 prompt ------------------------------------------;
-prompt ---------- insertion COLLECTE -------------;
+prompt ---------- insertion COLLECTE ------------;
 prompt ------------------------------------------;
 
 INSERT INTO COLLECTE VALUES (1,'10-03-2020','30-03-2020','30, Faculte des Sciences de Montpellier, Place E. Bataillon, 34095 Montpellier','NORMAL');
